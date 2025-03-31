d0b074cccf23d609fd2b163ae3ff2f3edb987b97
NotAkumpo <abdiel.evangelista@student.ateneo.edu>
2025-03-31 15:35:59 +0800

    Version 3: Finished algorithm for is_balanced

--- BalancedBrackets.py
+++ BalancedBrackets.py
@@ -1,16 +1,37 @@
 def is_balanced(expression):
     state = "q0"
-    stack = [ "Z" ]
-    position = 1
+    stack = ["Z"]
+    position = 0
+    extra = False
     displayExpression = expression
+
+    def pos_error_message():
+        print(f"Invalid string. Failed at position {position}.")
+        print(f"Remaining unprocessed input string: {expression}")
+
+    def state_error_message():
+        print(f"Invalid string. {state} is not a final state.")
+
+    def success_message():
+        print(f"{state} is a final state.")
+        print(f"{displayExpression} is valid and has balanced brackets.")
+
+    def process_message():
+        stringStack = "".join(stack)
+        print(f"ID: ({state}, {expression}, {stringStack})")
 
     print(f"Processing {expression}")
 
-    while (state != "q2" or expression):
-        process_message(state, expression, stack)
+    while expression != "E" and stack:
+        position += 1
+
+        if not expression:
+            expression = "E"
+
+        process_message()
         if state == "q0":
             if expression[0] == "!":
-                stack.append("!")
+                stack.insert(0, "!")
                 state = "q1"
                 expression = expression[1:]
             else:
@@ -20,74 +41,89 @@ def is_balanced(expression):
             if expression[0] == "x":
                 expression = expression[1:]
             elif expression[0] == "<":
-                stack.append("<")
+                stack.insert(0, "<")
                 expression = expression[1:]
             elif expression[0] == "{":
-                stack.append("{")
+                stack.insert(0, "{")
                 expression = expression[1:]
             elif expression[0] == "[":
-                stack.append("[")
+                stack.insert(0, "[")
                 expression = expression[1:]
             elif expression[0] == "(":
-                stack.append("(")
+                stack.insert(0, "(")
                 expression = expression[1:]
             elif expression[0] == ">":
                 if stack[0] == "<":
-                    stack.pop()
+                    stack.pop(0)
                     expression = expression[1:]
                 else:
                     pos_error_message()
                     break
             elif expression[0] == "}":
                 if stack[0] == "{":
-                    stack.pop()
+                    stack.pop(0)
                     expression = expression[1:]
                 else:
                     pos_error_message()
                     break
             elif expression[0] == "]":
                 if stack[0] == "[":
-                    stack.pop()
+                    stack.pop(0)
                     expression = expression[1:]
                 else:
                     pos_error_message()
                     break
             elif expression[0] == ")":
                 if stack[0] == "(":
-                    stack.pop()
+                    stack.pop(0)
                     expression = expression[1:]
                 else:
                     pos_error_message()
                     break
             elif expression[0] == "!":
                 if stack[0] == "!":
-                    stack.pop()
+                    stack.pop(0)
                     state = "q2"
                     expression = expression[1:]
                 else:
                     pos_error_message()
                     break
+            elif expression[0] == "E":
+                state_error_message()
+                break
             else:
                 pos_error_message()
                 break
                         
         elif state == "q2":
-            pass
-    
+            if expression != "E":
+                pos_error_message()
+                extra = True
+                break
+            else:
+                stack.pop(0)
 
-    def pos_error_message():
-        print(f"Invalid string. Failed at position {position}")
-        print(f"Remaining unprocessed input string: {expression}")
-
-    def state_error_message():
-        print(f"Invalid string. {state} is not a final state.")
-
-    def success_message():
-        print("{displayExpression} is valid and has balanced brackets.")
-
-    def process_message(state, expression, stack):
-        stringStack = "".join(stack)
-        print(f"ID: ({state}, {expression}, {stringStack})")
+    if state == "q2" and not extra:
+        success_message()
 
 # exp = input("Enter the expression: ")
 # is_balanced("john")
+
+# exp = "i"
+# exp = exp[1:]
+
+# if (exp):
+#     print(exp)
+# else:
+#     print("Fail")
+
+# is_balanced("!xx[x({xx})[xxx]x]<xxx>x!")
+# is_balanced("![<]>!")
+# is_balanced("![]")
+# is_balanced("!<a{[()]}a>!")
+# is_balanced("([]){}")
+is_balanced("!<x{[()]}x>!")
+
+# array = ["john", "alice"]
+# array.insert(0, "bob")
+# print(array[0])
